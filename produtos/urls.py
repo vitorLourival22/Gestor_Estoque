130bb92d4bbfff972b3cb4ab7eb56da711bfc3f5
vitorLourival22 <vitorLou2204@gmail.com>
2024-07-05 21:02:14 -0300

    atualização05/07/2024

--- produtos/urls.py
+++ produtos/urls.py
@@ -4,6 +4,7 @@ from .views import (
   adicionar_embalagens,
   adicionar_local,
   editar_locais,
+  excluir_locais,
   inicio,
   listar_embalagens,
   listar_locais,
@@ -14,7 +15,8 @@ urlpatterns = [
     # locais
     path('locais/', listar_locais, name='listar_locais'),
     path('locais/adicionar/', adicionar_local, name='adicionar_local'),
-    path('embalagem/', listar_embalagens, name='listar_embalagem'),
-    path('embalagem/adicionar', adicionar_embalagens, name='adicionar_embalagem'), 
-    path('editar_locais/<pk>/', editar_locais, name='editar_locais'),  # noqa: E501
+    path('embalagens/', listar_embalagens, name='listar_embalagens'),
+    path('embalagens/adicionar', adicionar_embalagens, name='adicionar_embalagens'),  # noqa: E501
+    path('editar_locais/<pk>/', editar_locais, name='editar_locais'),
+    path('excluir_locais/<pk>/', excluir_locais, name='excluir_locais'),  # noqa: E501
 ]
